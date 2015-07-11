d3dc4ae333094a8d8061f61052d9318c24981a68
TacticalFreak <RedKage@gmail.com>
2015-07-11 13:06:07 +0200

    * Fixed MK13 flash body and mark * renamed decals -> entities

--- update_extragear_folder.cmd
+++ update_extragear_folder.cmd
@@ -1,7 +1,9 @@
 @echo off
 set sourceFolder=.
 set targetFolder=C:\Users\RedShadow\Documents\KillHouseGames\DoorKickers\mods\redshadow_gear_extragear
+
 echo Updating...
-xcopy "%sourceFolder%" "%targetFolder%" /S /E /Q /V /C /G /H /R /Y
+rmdir /S /Q "%targetFolder%"
+xcopy "%sourceFolder%" "%targetFolder%" /S /E /I /Q /V /C /G /H /R /Y
 echo(
 pause
