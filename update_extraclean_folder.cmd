47bea379e9c23e20a65cd9b5c727e6a1da1e9dd8
TacticalFreak <RedKage@gmail.com>
2015-07-19 16:07:43 +0200

    License and placeholder

--- update_extraclean_folder.cmd
+++ update_extraclean_folder.cmd
@@ -1,7 +1,6 @@
 @echo off
 set sourceFolder=.
-set targetFolder=C:\Users\RedShadow\Documents\KillHouseGames\DoorKickers\mods\redshadow_other_cleanplay
-
+set targetFolder=C:\Users\RedShadow\Documents\KillHouseGames\DoorKickers\mods\redshadow_other_extraclean
 echo Updating...
 xcopy "%sourceFolder%" "%targetFolder%" /S /E /Q /V /C /G /H /R /Y
 echo(
